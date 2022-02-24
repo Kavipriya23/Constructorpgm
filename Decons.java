db20b34ef0ee68a2c0f9acc27a636e59e802c00e
Kavipriya23 <kavi16pretty@gmail.com>
2022-02-24 12:12:02 +0530

    Changed and Committed

---
+++ Decons.java
@@ -0,0 +1,23 @@
+package deconpro;
+class demo{
+			int id;
+			String name;
+
+		void display() {
+			System.out.println(id+" "+name);
+			}
+		}
+
+public class  Decons {
+
+    public static void main(String[] args) {
+
+    	demo d1=new demo();
+			demo d2=new demo ();
+
+			d1.display();
+			d2.display();
+			}
+		}
+
+		
